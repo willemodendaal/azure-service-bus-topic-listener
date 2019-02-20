db35ad77d964a66c2d7ff0583ea28cee2ae8f7e8
willemodendaal <willem@newmedialabs.co.za>
2019-02-20 11:42:39 +0200

    Introduced necessary 'subscription_name' parameter.

--- ServiceBusTopicListener/Program.cs
+++ ServiceBusTopicListener/Program.cs
@@ -20,11 +20,12 @@ namespace ServiceBusTopicListener
 			}
 
 			string topicName = args[0];
-			string connectionString = args[1];
+			string subscriptionName = args[1];
+			string connectionString = args[2];
 
 			try
 			{
-				var subscriptionClient = SubscriptionClient.CreateFromConnectionString(connectionString, topicName, "testApp1", ReceiveMode.ReceiveAndDelete);
+				var subscriptionClient = SubscriptionClient.CreateFromConnectionString(connectionString, topicName, subscriptionName, ReceiveMode.ReceiveAndDelete);
 				var options = new OnMessageOptions
 				{
 					AutoComplete = true,
@@ -34,21 +35,20 @@ namespace ServiceBusTopicListener
 				Console.WriteLine($"Listening for messages on topic '{topicName}'...");
 				Console.WriteLine("Press Ctrl+C to exit.");
 				subscriptionClient.OnMessage(ReceiveMessage, options);
+				Console.ReadLine();
 			}
 			catch (Exception exc)
 			{
 				Console.WriteLine($"General error. Exiting program.\n\nError Message: '{exc.Message}'\n\nStackTrace: '{exc.StackTrace}'");
 			}
-			Console.WriteLine("Press Enter to exit...");
-			Console.ReadLine();
 		}
 
 		private static void OutputProgramIntroText()
 		{
 			Console.WriteLine("ServiceBusTopicListener");
-			Console.WriteLine("Usage: ServiceBusTopicListener.exe <topicName> <connectionString>");
+			Console.WriteLine("Usage: ServiceBusTopicListener.exe <topicName> <subscriptionName> <connectionString>");
 			Console.WriteLine(
-				"eg: ServiceBusTopicListener.exe my_topic Endpoint=sb://mysb.servicebus.windows.net/;SharedAccessKeyName=tempReaderAccessKey;SharedAccessKey=xxxxx");
+				"eg: ServiceBusTopicListener.exe my_topic my_subscription Endpoint=sb://mysb.servicebus.windows.net/;SharedAccessKeyName=tempReaderAccessKey;SharedAccessKey=xxxxx");
 		}
 
 		private static void ReceiveMessage(BrokeredMessage receivedMessage)
@@ -69,7 +69,7 @@ namespace ServiceBusTopicListener
 
 		private static bool ArgsAreValid(string[] args)
 		{
-			if (args == null || args.Length != 2)
+			if (args == null || args.Length != 3)
 			{
 				return false;
 			}
@@ -80,9 +80,15 @@ namespace ServiceBusTopicListener
 				return false;
 			}
 
-			if (!Regex.IsMatch(args[1], ValidConnectionStringPattern))
+			if (!Regex.IsMatch(args[1], SubscriptionNamePattern))
 			{
-				Console.WriteLine($"Invalid service bus connection string '{args[1]}'.\n\n");
+				Console.WriteLine($"Invalid subscription name '{args[1]}'.\n\n");
+				return false;
+			}
+
+			if (!Regex.IsMatch(args[2], ValidConnectionStringPattern))
+			{
+				Console.WriteLine($"Invalid service bus connection string '{args[2]}'.\n\n");
 				return false;
 			}
 
@@ -90,6 +96,7 @@ namespace ServiceBusTopicListener
 		}
 
 		private static string ValidTopicNamePattern = "^[a-zA-Z0-9_\\-.]+$";
+		private static string SubscriptionNamePattern = "^[a-zA-Z0-9_\\-.]+$";
 		private static string ValidConnectionStringPattern = "^Endpoint.+SharedAccessKeyName.+SharedAccessKey.+=$";
 	}
 }
